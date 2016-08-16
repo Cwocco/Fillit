0bb0ea2e2ff7ac228ab27ac14d6106f5f83b992c
Cwocco <cwo.payp@gmail.com>
2016-08-16 05:31:22 +0200

    check_contact

--- src/t_read.c
+++ src/t_read.c
@@ -6,27 +6,46 @@
 /*   By: ada-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/14 14:44:03 by ada-cunh          #+#    #+#             */
-/*   Updated: 2016/08/14 18:02:10 by ada-cunh         ###   ########.fr       */
+/*   Updated: 2016/08/16 01:31:18 by ada-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft.h"
 
-int		ft_check_tetri(char *s)
+/*
+Verifie que les caracteres des tetriminos sont en contact avc un autre block
+Si la fonction return 4 ou 6, le tetriminos est valide
+*/
+
+int		ft_check_contact(char *s)
 {
 	int i;
+	int	contact;
 
 	i = 0;
+	tetri = 0;
 	while (i < 20)
 	{
-		while (s[i] == '#' && s[i + 1] == '#')
+		while (s[i] == '#')
 		{
-			
+			if (((s[i + 1] < 20) && (s[i + 1] == '#')) || ((s[i + 5] < 20) && (s[i + 5] == '#')))
+				contact++;
+			if (((s[i - 1] >= 0) && (s[i - 1] == '#')) || ((s[i - 5] >= 0) && (s[i - 5] == '#')))
+				contact++;
+			i++;
 		}
+		return (contact == 4 || contact == 6);
 	}
 }
 
+int		ft_check_tetri(char *s, int )
+{
+	int	i;
+
+	i = 0;
+
+}
 t_list ft_read(int fd)
 {
 	char *buf;
@@ -37,4 +56,3 @@ t_list ft_read(int fd)
 	if (!(buf = (char *)malloc(sizeof(char *) * (21))))
 		return (NULL);
 }
-
