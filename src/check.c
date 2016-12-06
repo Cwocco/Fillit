48dd275389c13f938605596fc900e516b14df74e
Cwocco <cwo.payp@gmail.com>
2016-12-06 23:18:26 +0100

    Correct smtg

--- src/check.c
+++ src/check.c
@@ -6,15 +6,40 @@
 /*   By: ada-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/01 05:02:08 by ada-cunh          #+#    #+#             */
-/*   Updated: 2016/12/01 05:07:51 by ada-cunh         ###   ########.fr       */
+/*   Updated: 2016/12/06 23:17:38 by ada-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-// J'suis fatigue j'essaye demain
 
 int superpose_check(list *tetrs, char **map, int map_size, int x, int y)
 {
+	int i;
+	int j;
+	
+	while (y < i)
+	{
+		while (x < j)
+		{
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+
+int try(list *tetrs, char **map)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	while (map)
+	{
+		
+	}
 	return (0);
 }
--- src/ft_solver.c
+++ src/ft_solver.c
@@ -6,7 +6,7 @@
 /*   By: nboste <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 20:45:34 by nboste            #+#    #+#             */
-/*   Updated: 2016/12/01 05:01:34 by ada-cunh         ###   ########.fr       */
+/*   Updated: 2016/12/06 22:42:41 by ada-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,34 +50,7 @@ void	reset_map(char **map)
 	}
 }
 
-/*
-char *ft_a_garder(list *lst, char **map)
-{
-	int i;
 
-	i = 0;
-	while (Tant que je suis dans lst et dans ma map)
-	{
-		if (Si jai bien lu mon tetriminos avec read)
-			Placer le tetriminos lu en (1,1) sur la map, ici anchor = 1 (faut surement 
-se servir  des struct dans types.h mais cest pas clair dans ma tete
-			 if (Jai pu placer mon tetriminos)
-              read le prochain tetriminos et le placer de facon a ce que lair de 
-              la map soit le plus petit possible
-		     if(lair actuelle est plus petite que lair precedente)
-            	continuer de read et de placer
-			else ()
-				 {
-					 recursive avc anchor + 1
-			des que laire actuelle redeviens plus petite que la precedente continuer de
-			 read et de placer (ici jcpa si on re initialise anchor, jpense que ui)
-					 chaque fois qu on incremente anchor faut comparer aire actuel a
-					 aire precedente et des qu on a la plus petite aire
-				 }
-		quand ya plus rien a read et si on a la plus petite aire on renvoi la map
-	}				
-}
-*/
 
 
 list *backtrack(list *tetrs, char **map, int map_size)
@@ -94,16 +67,16 @@ list *backtrack(list *tetrs, char **map, int map_size)
 	{
 		while (map && (x < map_size))
 		{
-			if (add_tetrs(tetrs, map, anchor, map_size))
+			if (add_tetrs(tetrs, map, x, y, map_size))
 			{
 				backtrack(tetrs->next, map, map_size);
-				del_tetrs(tetris, map, anchor, map_size);
+				del_tetrs(tetris, map, x, y, map_size);
 			}
 			x++;
 		}
 		y++;
 	}
-	// Ici j'voudrais faire remplir la map solve pour pouvor la print
+	// Ici j'voudrais faire remplir la map solve pour pouvoir la print
 	return (map);
 }
 
